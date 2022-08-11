bd03f917425e1e87363058b433b682667bfe90f3
Ben <ben.a.burgos@gmail.com>
2022-08-10 20:19:20 -0700

    adds initial login/logout functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import LoginButton from './components/login';
+import LogoutButton from './components/logout';
+import Profile from './components/profile';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello world!</h1>
+      <LoginButton /> <LogoutButton />
+      <Profile />
     </div>
   );
 }
