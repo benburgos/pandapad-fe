40e8f47931357e1b1dddf9001d92d88c6eaa79b0
Ben <ben.a.burgos@gmail.com>
2022-08-10 20:34:50 -0700

    changes login window into a pop-up

--- src/components/login.js
+++ src/components/login.js
@@ -1,9 +1,9 @@
 import { useAuth0 } from '@auth0/auth0-react';
 
 const LoginButton = () => {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithPopup } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  return <button onClick={() => loginWithPopup()}>Log In</button>;
 };
 
 export default LoginButton;
