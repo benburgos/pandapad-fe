d076c30d05ab8c04fe1421e23bd37abe8eb6b526
Ben <ben.a.burgos@gmail.com>
2022-08-10 21:41:09 -0700

    adds some functionality to create-update-delete

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,26 +1,45 @@
 import { useEffect, useState } from 'react';
 
 const Main = () => {
-    const [data, setData] = useState(null);
+  const [ticket, setTicket] = useState(null);
 
   const URL = 'http://localhost:8000/tickets';
 
-  async function getData() {
-    const response = await fetch(URL).then(response => response.json())
-    setData(response)
+  async function getTickets() {
+    const response = await fetch(URL).then((response) => response.json());
+    setTicket(response);
+  }
+
+  async function createTicket(ticket) {
+    await fetch(`${URL}/create'`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'Application/json' },
+      body: JSON.stringify(ticket),
+    });
+    getTickets();
+  }
+
+  async function updateTicket(ticket, id) {
+    await fetch(`${URL}/tickets/edit?ticketID=${id}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(ticket),
+    });
+    getTickets();
+  }
+
+  async function deleteTicket(id) {
+    await fetch(`${URL}/tickets/delete?ticketID=${id}`, {
+      method: 'DELETE',
+    });
+    getTickets();
   }
 
   useEffect(() => {
-    getData()
+    getTickets();
   }, []);
 
-  return(
-    <ul>
-        {data.map(e => (
-            <li key={e.id}>{e.subject}</li>
-        ))}
-    </ul>
-  );
+  return <h1>Main Component</h1>;
 };
 
 export default Main;
