835e63acb89104576e22191503d3a62340ccab38
Ben <ben.a.burgos@gmail.com>
2022-08-13 16:21:44 -0700

    adds hardcorded cards

--- src/routes/conversation.js
+++ src/routes/conversation.js
@@ -1,17 +1,52 @@
-import './conversation.css'
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faCommentDots
+} from '@fortawesome/free-solid-svg-icons';
+import './conversation.css';
 
 export default function Conversation() {
   return (
-    <div className='conversation-box'>
+    <div className="conversation-box">
       <div className="header">
         <h2>Conversation List</h2>
       </div>
 
       <hr />
 
-      <div className='conversation-list'>
+      <div className="conversation-list">
+        <div className='conversation-list-card'>
+            <ul>
+            <div className="conversation-list-user">
+                <li>Icon</li>
+                <li>Name</li>
+            </div>
 
+            <div className="conversation-list-comment">
+                <Link to='/unavailable'>
+                    <li>My stuff is on fire man!</li>
+                    <li><FontAwesomeIcon icon={faCommentDots} /></li>
+                </Link>
+            </div>
+            </ul>
+        </div>
+        <div className='conversation-list-card'>
+            <ul>
+            <div className="conversation-list-user">
+                <li>Icon</li>
+                <li>Name</li>
+            </div>
+
+            <div className="conversation-list-comment">
+                <Link to='/unavailable'>
+                    <li>My stuff is on fire man!</li>
+                    <li><FontAwesomeIcon icon={faCommentDots} /></li>
+                </Link>
+            </div>
+            </ul>
+        </div>
       </div>
+
     </div>
   );
 }
