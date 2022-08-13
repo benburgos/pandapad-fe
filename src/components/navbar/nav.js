57a21dde50ba0bcfaefc1899048ae3b2af15e69b
Ben <ben.a.burgos@gmail.com>
2022-08-13 15:12:58 -0700

    adds icons

--- src/components/navbar/nav.js
+++ src/components/navbar/nav.js
@@ -1,8 +1,31 @@
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faSignInAlt,
+  faComment,
+  faEnvelope,
+  faPhone,
+  faIdCard
+} from '@fortawesome/free-solid-svg-icons';
 
 export default function Navbar() {
-  return <>
-  <Link>
-  </Link>
-  </>;
+  return (
+    <>
+      <Link to='/'>
+      <FontAwesomeIcon icon={faPhone} flip="horizontal" />
+      </Link>
+      <Link to='/'>
+      <FontAwesomeIcon icon={faEnvelope} flip="horizontal" />
+      </Link>
+      <Link to='/'>
+      <FontAwesomeIcon icon={faComment} flip="horizontal" />
+      </Link>
+      <Link to='/'>
+      <FontAwesomeIcon icon={faIdCard} flip="horizontal" />
+      </Link>
+      <Link to='/'>
+      <FontAwesomeIcon icon={faSignInAlt} flip="horizontal" />
+      </Link>
+    </>
+  );
 }
