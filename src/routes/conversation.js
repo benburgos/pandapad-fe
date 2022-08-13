e126b9f96d82e62e3e0619f10af7c1db4fca3f29
Ben <ben.a.burgos@gmail.com>
2022-08-13 16:04:22 -0700

    initial conversation styling

--- src/routes/conversation.js
+++ src/routes/conversation.js
@@ -1,7 +1,17 @@
+import './conversation.css'
+
 export default function Conversation() {
-    return (
-        <div>
-            <h1>Conversation List</h1>
-        </div>
-    )
+  return (
+    <div className='conversation-box'>
+      <div className="header">
+        <h2>Conversation List</h2>
+      </div>
+
+      <hr />
+
+      <div className='conversation-list'>
+
+      </div>
+    </div>
+  );
 }
