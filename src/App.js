f34917bd5fd87d7f17b87832a8525327a88a964b
Ben <ben.a.burgos@gmail.com>
2022-08-13 15:00:06 -0700

    adds fontawesome

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import './App.css';
-import LoginButton from './components/button/login';
-import LogoutButton from './components/button/logout';
-import Main from './components/Main';
 import Navbar from './components/navbar/nav';
-import Profile from './components/profile';
 
 function App() {
   return (
