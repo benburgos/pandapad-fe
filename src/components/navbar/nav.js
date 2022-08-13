221865920bea95c1320232dd268ff28916dcd222
Ben <ben.a.burgos@gmail.com>
2022-08-13 15:25:51 -0700

    styles navbar

--- src/components/navbar/nav.js
+++ src/components/navbar/nav.js
@@ -7,25 +7,27 @@ import {
   faPhone,
   faIdCard
 } from '@fortawesome/free-solid-svg-icons';
+import './nav.css'
 
 export default function Navbar() {
   return (
-    <>
+    <nav className='navbar'>
+        <img alt='pandapad-logo' src='https://www.freepnglogos.com/uploads/panda-png/panda-thomas-dafoe-studios-bare-bears-png-pack-3.png'/>
       <Link to='/'>
-      <FontAwesomeIcon icon={faPhone} flip="horizontal" />
+      <FontAwesomeIcon icon={faPhone} flip="horizontal" /> Talk
       </Link>
       <Link to='/'>
-      <FontAwesomeIcon icon={faEnvelope} flip="horizontal" />
+      <FontAwesomeIcon icon={faEnvelope} flip="horizontal" /> Mail
       </Link>
       <Link to='/'>
-      <FontAwesomeIcon icon={faComment} flip="horizontal" />
+      <FontAwesomeIcon icon={faComment} flip="horizontal" /> Conversations
       </Link>
       <Link to='/'>
-      <FontAwesomeIcon icon={faIdCard} flip="horizontal" />
+      <FontAwesomeIcon icon={faIdCard} flip="horizontal" /> Profile
       </Link>
       <Link to='/'>
-      <FontAwesomeIcon icon={faSignInAlt} flip="horizontal" />
+      <FontAwesomeIcon icon={faSignInAlt} flip="horizontal" /> Logout
       </Link>
-    </>
+    </nav>
   );
 }
