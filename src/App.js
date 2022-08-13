a101ee42517c3f26752d0826b566d2918b7f6718
Ben <ben.a.burgos@gmail.com>
2022-08-13 14:48:02 -0700

    boilertext for styling

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import LoginButton from './components/button/login';
 import LogoutButton from './components/button/logout';
 import Main from './components/Main';
+import Navbar from './components/navbar/nav';
 import Profile from './components/profile';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
     </div>
   );
 }
