0dd99f16727d914b169c86ca704e494adf831433
Ben <ben.a.burgos@gmail.com>
2022-08-13 15:33:16 -0700

    adds initial navbar

--- src/components/navbar/nav.js
+++ src/components/navbar/nav.js
@@ -13,19 +13,19 @@ export default function Navbar() {
   return (
     <nav className='navbar'>
         <img alt='pandapad-logo' src='https://www.freepnglogos.com/uploads/panda-png/panda-thomas-dafoe-studios-bare-bears-png-pack-3.png'/>
-      <Link to='/'>
+      <Link to='/unavailable'>
       <FontAwesomeIcon icon={faPhone} flip="horizontal" /> Talk
       </Link>
-      <Link to='/'>
+      <Link to='/unavailable'>
       <FontAwesomeIcon icon={faEnvelope} flip="horizontal" /> Mail
       </Link>
-      <Link to='/'>
+      <Link to='/conversation'>
       <FontAwesomeIcon icon={faComment} flip="horizontal" /> Conversations
       </Link>
-      <Link to='/'>
+      <Link to='/unavailable'>
       <FontAwesomeIcon icon={faIdCard} flip="horizontal" /> Profile
       </Link>
-      <Link to='/'>
+      <Link to='/unavailable'>
       <FontAwesomeIcon icon={faSignInAlt} flip="horizontal" /> Logout
       </Link>
     </nav>
