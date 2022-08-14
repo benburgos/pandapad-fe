6d8fe3a27abb68cac666aabaab2a8056b4f06c89
Ben <ben.a.burgos@gmail.com>
2022-08-13 20:39:42 -0700

    finishes convo list

--- src/routes/conversation.js
+++ src/routes/conversation.js
@@ -1,9 +1,25 @@
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCommentDots, faUserCircle, faPlusSquare } from '@fortawesome/free-solid-svg-icons';
+import {
+  faCommentDots,
+  faUserCircle,
+  faPlusSquare,
+} from '@fortawesome/free-solid-svg-icons';
 import './conversation.css';
 
 export default function Conversation() {
+  const [convoList, setConvoList] = useState([]);
+
+  useEffect(() => {
+    async function getConvos() {
+      const response = await fetch('http://localhost:8000/tickets');
+      const data = await response.json();
+      setConvoList(data);
+    }
+    getConvos();
+  }, []);
+
   return (
     <div className="conversation-box">
       <div className="header">
@@ -14,51 +30,32 @@ export default function Conversation() {
       <hr />
 
       <div className="conversation-list">
-        <div className="conversation-list-card">
-          <div className="conversation-list-user">
-            <span>
-              <FontAwesomeIcon icon={faUserCircle} />
-            </span>
-            <span>Jeff Goldblum</span>
-          </div>
+        {convoList.map((convo, index) => {
+          return (
+            <div className="conversation-list-card" key={index}>
+              <div className="conversation-list-user">
+                <span>
+                  <FontAwesomeIcon icon={faUserCircle} />
+                </span>
+                <span>{convo.from}</span>
+              </div>
 
-          <div className="conversation-list-subject">
-            <Link to="/conversation/detail">
-              <span>My stuff is on fire man!</span>
-            </Link>
-          </div>
+              <div className="conversation-list-subject">
+                <Link to="/conversation/detail">
+                  <span>{convo.body}</span>
+                </Link>
+              </div>
 
-          <div className="conversation-list-icon">
-            <Link to="/conversation/detail">
-              <span>
-                <FontAwesomeIcon icon={faCommentDots} />
-              </span>
-            </Link>
-          </div>
-        </div>
-
-        <div className="conversation-list-card">
-          <div className="conversation-list-user">
-            <span>
-              <FontAwesomeIcon icon={faUserCircle} />
-            </span>
-            <span>Jeff Goldblum</span>
-          </div>
-
-          <div className="conversation-list-subject">
-            <Link to="/conversation/detail">
-              <span>My stuff is on fire man!</span>
-            </Link>
-          </div>
-
-          <div className="conversation-list-icon">
-            <Link to="/conversation/detail">
-              <span>
-                <FontAwesomeIcon icon={faCommentDots} />
-              </span>
-            </Link>
-          </div>
-        </div>
+              <div className="conversation-list-icon">
+                <Link to="/conversation/detail">
+                  <span>
+                    <FontAwesomeIcon icon={faCommentDots} />
+                  </span>
+                </Link>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
