8a11459c5e5b21bb0b0d709bde06c1d4c3e2fc01
Ben <ben.a.burgos@gmail.com>
2022-08-14 21:25:31 -0700

    changes up auth logic

--- src/App.js
+++ src/App.js
@@ -10,24 +10,24 @@ import './App.css';
 function App() {
   const { isAuthenticated } = useAuth0();
 
-  if (isAuthenticated) {
-    return (
-      <div className="App">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Conversation />} />
-          <Route path="/unavailable" element={<Unavailable />} />
-          <Route path="/conversation/detail/:id" element={<ConversationDetail />} />
-        </Routes>
-      </div>
-    );
-  } else {
+  if (!isAuthenticated) {
     return (
       <div className='splash-page'>
         <h1>Welcome to Pandapad!</h1>
         <img alt='pandapad-logo' src='https://www.freepnglogos.com/uploads/panda-png/panda-thomas-dafoe-studios-bare-bears-png-pack-3.png'/>
         <LoginButton />
       </div>
+    );
+  } else {
+    return (
+      <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Conversation />} />
+        <Route path="/unavailable" element={<Unavailable />} />
+        <Route path="/conversation/detail/:id" element={<ConversationDetail />} />
+      </Routes>
+    </div>
     )
   }
 }
