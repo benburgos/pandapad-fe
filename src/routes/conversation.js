9f256b6dff150dcb4c60f764cea96349b6e158c9
Ben <ben.a.burgos@gmail.com>
2022-08-14 21:23:05 -0700

    attempted fix for auth load

--- src/routes/conversation.js
+++ src/routes/conversation.js
@@ -1,5 +1,5 @@
 import { useAuth0 } from '@auth0/auth0-react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
@@ -47,75 +47,71 @@ export default function Conversation() {
     }
   };
 
-  if (convoList) {
-    return (
-      <div className="conversation-box">
-        <div className="header">
-          <h2>Conversation List</h2>
-          <Popup trigger={<FontAwesomeIcon icon={faPlusSquare} />} modal>
-            {(close) => (
-              <div className="modal">
-                <button className="close" onClick={close}>
-                  &times;
-                </button>
-                <div className="header"> New Ticket </div>
-                <div className="content">
-                  <form onSubmit={handleSubmit}>
-                    To:{' '}
-                    <input
-                      type="text"
-                      value={msgTo}
-                      onChange={(e) => setMsgTo(e.target.value)}
-                      required
-                    />
-                    Message Body:{' '}
-                    <input
-                      type="text"
-                      value={msgBody}
-                      onChange={(e) => setMsgBody(e.target.value)}
-                      required
-                    />
-                    <input type="submit" />
-                  </form>
-                </div>
+  return (
+    <div className="conversation-box">
+      <div className="header">
+        <h2>Conversation List</h2>
+        <Popup trigger={<FontAwesomeIcon icon={faPlusSquare} />} modal>
+          {(close) => (
+            <div className="modal">
+              <button className="close" onClick={close}>
+                &times;
+              </button>
+              <div className="header"> New Ticket </div>
+              <div className="content">
+                <form onSubmit={handleSubmit}>
+                  To:{' '}
+                  <input
+                    type="text"
+                    value={msgTo}
+                    onChange={(e) => setMsgTo(e.target.value)}
+                    required
+                  />
+                  Message Body:{' '}
+                  <input
+                    type="text"
+                    value={msgBody}
+                    onChange={(e) => setMsgBody(e.target.value)}
+                    required
+                  />
+                  <input type="submit" />
+                </form>
               </div>
-            )}
-          </Popup>
-        </div>
-
-        <hr />
-
-        <div className="conversation-list">
-          {convoList.map((convo, index) => {
-            return (
-              <div className="conversation-list-card" key={index}>
-                <div className="conversation-list-user">
-                  <span>
-                    <FontAwesomeIcon icon={faUserCircle} />
-                  </span>
-                  <span>{convo.from}</span>
-                </div>
-
-                <div className="conversation-list-subject">
-                  <Link to={`/conversation/detail/${convo._id}`}>
-                    <span>{convo.body}</span>
-                  </Link>
-                </div>
-
-                <div className="conversation-list-icon">
-                  <Link to={`/conversation/detail/${convo._id}`}>
-                    <span>
-                      <FontAwesomeIcon icon={faCommentDots} />
-                    </span>
-                  </Link>
-                </div>
-              </div>
-            );
-          })}
-        </div>
+            </div>
+          )}
+        </Popup>
       </div>
-    );
-  } else {
-    return <h1>Loading..</h1>;
-  }
+
+      <hr />
+
+      <div className="conversation-list">
+        {convoList.map((convo, index) => {
+          return (
+            <div className="conversation-list-card" key={index}>
+              <div className="conversation-list-user">
+                <span>
+                  <FontAwesomeIcon icon={faUserCircle} />
+                </span>
+                <span>{convo.from}</span>
+              </div>
+
+              <div className="conversation-list-subject">
+                <Link to={`/conversation/detail/${convo._id}`}>
+                  <span>{convo.body}</span>
+                </Link>
+              </div>
+
+              <div className="conversation-list-icon">
+                <Link to={`/conversation/detail/${convo._id}`}>
+                  <span>
+                    <FontAwesomeIcon icon={faCommentDots} />
+                  </span>
+                </Link>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
